14550ab427e4f052b99db71ba332548fbad1274c
Ingrid Rosselis <irscunha@dcc.ufmg.br>
2018-04-29 22:32:49 -0300

    new test cases and pivoting method

--- tp1/src/main.py
+++ tp1/src/main.py
@@ -7,24 +7,16 @@ matrix = Matrix()
 
 def main():
     start()
-    matrix.fpi()
-    print("fpi:")
-    print(matrix.fpi)
-    matrix.tableau()
-    print("tableau:")
-    print(matrix.tableau)
-    matrix.auxiliary()
-    print("auxiliar:")
-    print(matrix.auxiliary)
-    matrix.pivot(1, 2)
+    matrix.default()
+    matrix.updateFractions()
 
 
 def start():
-    file = open(filename, "r")
+    file = open(filename, 'r')
     row = int(file.readline())
     col = int(file.readline())
-    aux1 = file.readline()
-    matrix.init(aux1, row, col)
+    aux = file.readline()
+    matrix.init(aux, row, col)
 
 
 if __name__ == '__main__':
--- tp1/src/matrix.py
+++ tp1/src/matrix.py
@@ -1,35 +1,33 @@
+from fractions import Fraction
 import numpy as np
 
 
 class Matrix:
-    A = ""
-    b = ""
-    c = ""
-    base = ""
-    fpi = ""
-    tableau = ""
-    auxiliary = ""
+    A = 0
+    b = 0
+    c = 0
+    matrix = 0
+    tableau = 0
 
     def init(self, m, row, col):
-        aux1 = np.matrix(m)
+        aux1 = np.matrix(m, dtype="object")
         aux2 = aux1.reshape((row + 1, col + 1))
 
-        self.A = np.matrix(aux2[1:, 0:-1])
-        self.b = np.matrix(aux2[:, -1])
-        self.c = np.matrix(aux2[0, 0:-1])
+        self.A = np.matrix(aux2[1:, 0:-1], dtype="object")
+        self.b = np.matrix(aux2[:, -1], dtype="object")
+        self.c = np.matrix(aux2[0, 0:-1], dtype="object")
 
-    def fpi(self):
-        zeros = np.zeros((1, self.A.shape[0]))
-        identity = np.identity(self.A.shape[0])
+        zeros = np.zeros((1, self.A.shape[0]), dtype="object")
+        identity = np.identity(self.A.shape[0], dtype="object")
 
         c1 = np.concatenate((self.c, zeros), axis=1)
         a1 = np.concatenate((self.A, identity), axis=1)
         a2 = np.concatenate((c1, a1), axis=0)
-        self.fpi = np.concatenate((a2, self.b), axis=1)
+        self.matrix = np.concatenate((a2, self.b), axis=1)
 
-    def tableau(self):
-        zeros = np.zeros((1, self.A.shape[0]))
-        identity = np.identity(self.A.shape[0])
+    def default(self):
+        zeros = np.zeros((1, self.A.shape[0]), dtype="object")
+        identity = np.identity(self.A.shape[0], dtype="object")
 
         c1 = -1 * self.c
         c2 = np.concatenate((zeros, c1, zeros), axis=1)
@@ -38,28 +36,43 @@ class Matrix:
         self.tableau = np.concatenate((a2, self.b), axis=1)
 
     def auxiliary(self):
-        zeros = np.zeros((1, self.A.shape[0]))
-        identity = np.identity(self.A.shape[0])
+        zeros = np.zeros((1, self.A.shape[0]), dtype="object")
+        identity = np.identity(self.A.shape[0], dtype="object")
 
-        c1 = np.ones((1, self.A.shape[0]))
-        c2 = np.zeros((1, self.c.shape[1]))
+        c1 = np.ones((1, self.A.shape[0]), dtype="object")
+        c2 = np.zeros((1, self.c.shape[1]), dtype="object")
         c3 = np.concatenate((zeros, c2, c1), axis=1)
         a1 = np.concatenate((identity, self.A, identity), axis=1)
         a2 = np.concatenate((c3, a1), axis=0)
-        self.auxiliary = np.concatenate((a2, self.b), axis=1)
+        self.tableau = np.concatenate((a2, self.b), axis=1)
 
     def pivot(self, row, col):
-        multiplier = 0
-        for i in self.tableau.T[col]:
-            print(i)
+        if(row > self.tableau.shape[0] or col > self.tableau.shape[1]):
+            print("Invalid position in tableau!")
+            return
 
-        print("pivot")
+        multiplier = np.zeros(self.tableau.shape[0], dtype="object")
+        multiplier[row] = Fraction(1, self.tableau[row, col])
+
+        for i in range(0, self.tableau.shape[1]):
+            self.tableau[row, i] = self.tableau[row, i] * multiplier[row]
+
+        for i in range(0, self.tableau.shape[0]):
+            if(i != row):
+                multiplier[i] = -1 * Fraction(self.tableau.T[col, i], self.tableau.T[col, row])
+
+        for i in range(0, self.tableau.shape[0]):
+            if(i != row):
+                for j in range(0, self.tableau.shape[1]):
+                    self.tableau[i, j] = self.tableau[i, j] + self.tableau[row, j] * multiplier[i]
 
     def solution(self):
-        print("solution")
+        print("sol")
 
-    def updateValues(self):
-        print("fractions")
+    def updateFractions(self):
+        for i in range(0, self.tableau.shape[0]):
+            for j in range(0, self.tableau.shape[1]):
+                self.tableau[i, j] = Fraction(self.tableau[i, j], 1)
 
     def updateBase(self):
         print("base")
