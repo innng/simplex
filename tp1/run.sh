51683ac34b703c342eb57be4ff4ffcecc9066322
Ingrid Rosselis <irscunha@dcc.ufmg.br>
2018-05-05 15:02:27 -0300

    improve run.sh

--- tp1/run.sh
+++ tp1/run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+if [ -f pivoting.txt ]; then
+    rm pivoting.txt;
+fi
+if [ -f conclusao.txt ]; then
+    rm pivoting.txt;
+fi
 python3 src/main.py src/tableau.py src/simplex.py src/utils.py $1
+rm -r src/__pycache__/
